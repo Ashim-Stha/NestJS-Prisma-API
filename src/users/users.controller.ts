49bbb0bbaaf60f1cef3ef73310ce14756103fe28
Ashim-Stha <ashimshrestha2384@gmail.com>
2024-06-28 08:56:52 +0545

    init

---
+++ src/users/users.controller.ts
@@ -0,0 +1,5 @@
+import { Controller } from '@nestjs/common';
+
+//controllers responsible for handling incoming requests and returning responses to the client
+@Controller('users')
+export class UsersController {}
