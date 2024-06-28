7021da956f8dd3f10a04d570dc18a718575e2d52
Ashim-Stha <ashimshrestha2384@gmail.com>
2024-06-28 09:23:11 +0545

    controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Body,
+  Post,
+  Patch,
+  Delete,
+  Query,
+} from '@nestjs/common';
 
 //controllers responsible for handling incoming requests and returning responses to the client
-@Controller('users')
-export class UsersController {}
+@Controller('users') // handles /users route
+export class UsersController {
+  //based on waterfall model - order matters
+
+  @Get() // GET /users or /users?role=value
+  findAll(@Query('role') role?: 'ADMIN' | 'ENGINEER' | 'INTERN') {
+    return [];
+  }
+
+  //   @Get('interns') // GET /users/interns
+  //   findAllInterns() {
+  //     return [];
+  //   }
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST /users
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
