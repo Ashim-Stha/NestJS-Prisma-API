f8c9795080f5b323d60af4fa532d867b1fedf6d6
Ashim-Stha <ashimshrestha2384@gmail.com>
2024-06-29 12:37:53 +0545

    dto validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsEnum, IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsEnum(['ADMIN', 'ENGINEER', 'INTERN'], {
+    message: 'Valid role required',
+  })
   role: 'ADMIN' | 'ENGINEER' | 'INTERN';
 }
